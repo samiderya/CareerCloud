0cb20daea9b2d506ea1eccf467c1400b61b1b586
sami yusuf <sami_yusuf@hotmail.com>
2017-12-10 15:59:40 +0200

    We create CareerCloud Project  with unit test

---
+++ CareerCloud.Pocos/CompanyJobEducationPoco.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CareerCloud.Pocos
+{
+   public class CompanyJobEducationPoco : IPoco
+    {
+        public Guid Id { get; set; }
+    }
+}
