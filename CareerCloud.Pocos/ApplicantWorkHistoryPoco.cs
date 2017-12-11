77aa72b5f17a3648d7aa1c9ad635196c2f284d40
sami yusuf <sami_yusuf@hotmail.com>
2017-12-11 17:35:52 +0200

    add field to the classes

--- CareerCloud.Pocos/ApplicantWorkHistoryPoco.cs
+++ CareerCloud.Pocos/ApplicantWorkHistoryPoco.cs
@@ -9,5 +9,16 @@ namespace CareerCloud.Pocos
    public class ApplicantWorkHistoryPoco : IPoco
     {
         public Guid Id { get; set; }
+        public Guid Applicant { get; set; }
+        public string CompanyName { get; set; }
+        public string CountryCode { get; set; }
+        public string Location { get; set; }
+        public string JobTitle { get; set; }
+        public string JobDescription { get; set; }
+        public byte StartMonth { get; set; }
+        public byte EndMonth { get; set; }
+        public int StartYear { get; set; }
+        public int EndYear { get; set; }
+        public byte TimeStamp { get; set; }
     }
 }
--- CareerCloud.Pocos/ApplicatSkillPoco.cs
+++ CareerCloud.Pocos/ApplicatSkillPoco.cs
@@ -9,5 +9,13 @@ namespace CareerCloud.Pocos
    public class ApplicatSkillPoco : IPoco
     {
         public Guid Id { get; set; }
+        public Guid Applicant { get; set; }
+        public string Skill { get; set; }
+        public string SkillLevel { get; set; }
+        public byte StartMonth { get; set; }
+        public byte StartYear { get; set; }
+        public byte EndMonth { get; set; }
+        public byte EndYear { get; set; }
+        public byte TimeStamp { get; set; }
     }
 }
